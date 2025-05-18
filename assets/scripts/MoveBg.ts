5b1b2a4fae8b84e27018637d6533840689c16ec8
huangyix <huangyix@gmail.com>
2025-05-18 20:54:10 +0800

    添加背景和地板，并控制移动和衔接

---
+++ assets/scripts/MoveBg.ts
@@ -0,0 +1,42 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('MoveBg')
+export class MoveBg extends Component {
+
+    @property(Node)
+    target1ToMove:Node = null;
+
+    @property(Node)
+    target2ToMove:Node = null;
+
+    @property
+    moveSpeed:number = 100;
+
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+        const moveDistance = this.moveSpeed*deltaTime;
+
+        let p1 = this.target1ToMove.getPosition();
+        this.target1ToMove.setPosition(p1.x-moveDistance,p1.y);
+
+        let p2 = this.target2ToMove.getPosition();
+        this.target2ToMove.setPosition(p2.x-moveDistance,p2.y);
+
+        if(p1.x < -730 ){
+            p2 = this.target2ToMove.getPosition();
+            this.target1ToMove.setPosition(p2.x+728,p2.y);
+        }
+
+        if(p2.x < -730 ){
+            p1 = this.target1ToMove.getPosition();
+            this.target2ToMove.setPosition(p1.x+728,p1.y);
+        }
+    }
+}
+
+
